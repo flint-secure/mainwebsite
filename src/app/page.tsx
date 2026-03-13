e39de3a85a10cee60381e6c95d218a6030aa4d9e
Aaditya B <150529501+aadityabinodyadav@users.noreply.github.com>
2026-03-13 21:08:27 +0545

    Comment out Founders component in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,7 +31,7 @@ export default function Home() {
         <Capabilities />
         <Integration />
         <Performance />
-        <Founders />
+      //  <Founders />
         <Pricing />
       </main>
       <Footer />
